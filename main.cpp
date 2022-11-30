4484c3d218974504e878824e8c255c48f125b509
Nixon Deb Antu <107603929+nixondebantu@users.noreply.github.com>
2022-11-30 23:05:45 +0600

    intro modified

--- main.cpp
+++ main.cpp
@@ -441,10 +441,10 @@ void Boarder()
 void Print()
 {
    //GotoXY(10,12);
-   printf("\tWelcome to the mini Snake game.(press any key to continue)\n");
+   printf("%c\tWelcome to the mini Snake game.\n\n\tModified by: Nixon Deb Antu\n\tRegistration No: 2020831023\n\n\t(press any key to continue)\n",7);
   getch();
    system("cls");
-   printf("\tGame instructions:\n");
+   printf("%c\tGame instructions:\n",7);
    printf("\n-> Use arrow keys to move the snake.\n\n-> You will be provided foods at the several coordinates of the screen which you have to eat. Everytime you eat a food the length of the snake will be increased by 1 element and thus the score.\n\n-> Here you are provided with three lives. Your life will decrease as you hit the wall or snake's body.\n\n-> YOu can pause the game in its middle by pressing any key. To continue the paused game press any other key once again\n\n-> If you want to exit press esc. \n");
    printf("\n\nPress any key to play game...");
    if(getch()==27)
